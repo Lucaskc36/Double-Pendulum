ce4cdf4dfb11e733888cfef736b1e91b9ef86c34
Korey <kpatriots8@gmail.com>
2025-03-20 10:27:03 -0600

    Caclulate positions, relative positions

--- doublePendulum.py
+++ doublePendulum.py
@@ -1,21 +1,33 @@
 import pygame
+import numpy as np
 # Initialize Pygame
 pygame.init()
 
+clock = pygame.time.Clock()
 # Set up the display window with a width of 800 pixels and a height of 600 pixels
 screen = pygame.display.set_mode((800, 600))
 origin = (400, 0)
-l = 300
+l = 150
 r_1 = (0,l)
-r_2 = (l,r_1[1])
-m_1 = 1
-m_2 = 1
-g = 9.8
-theta_1 = 0
-theta_2 = 0
-theta_1_dot = 0
-theta_2_dot = 0
+r_2 = (r_1[0],r_1[1]+l)
 
+m_1 = 1 #kg
+m_2 = 1 #kg
+g = 9.8 #m/s^2
+phi_1 = 0 #radians
+phi_2 = 0 #radians
+phi_1_dot = 0 #radians/s
+phi_2_dot = 0 #radians/s
+
+
+
+def calculate_position(phi_1, phi_2):
+    x_1 = l * np.sin(phi_1)
+    y_1 = l * np.cos(phi_1)
+    x_2 = x_1 + l * np.sin(phi_2)
+    y_2 = y_1 + l * np.cos(phi_2)
+    return (x_1,y_1), (x_2,y_2)
+#in_range: checks if the position given is in range of the pendulum
 def in_range(pos: tuple, origin: tuple,l: int):
     return (pos[0] - origin[0])**2 + (pos[1] - origin[1])**2 <= l**2
 
@@ -25,15 +37,17 @@ pygame.display.set_caption("Double Pendulum")
 # Main loop
 running = True
 while running:
+    ## Calculates the delta time
+    delta_time = clock.tick(60) / 1000  # pygame.tick() returns milliseconds, so divide by 1000 to get seconds
     for event in pygame.event.get():
         # Quit the window if the user closes it
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-        ## Cursor position
+            ## Cursor position
             cursor_pos = pygame.mouse.get_pos()
             print("Mouse button down at", cursor_pos)
-            if in_range(cursor_pos, origin, l):
+            if in_range(cursor_pos, origin, 2*l):
                 print("In range")
             else:
                 print("Out of range")
@@ -42,14 +56,20 @@ while running:
             print("Mouse button up at", cursor_pos)
         if event.type == pygame.MOUSEMOTION:
             cursor_pos = pygame.mouse.get_pos()
+            if in_range(cursor_pos, origin, 2*l):
+                print("In range")
+            else:
+                print("Out of range")
             print("Mouse moved to", cursor_pos)
 
-
-    pygame.draw.line(screen, (207, 184, 124), (400, 0), (400, 300), 5)
-    pygame.draw.line(screen, (207, 184, 124), (400, 300), (400, 500), 5)
-    pygame.draw.circle(screen, (255, 255, 255), (400, 300), 10)
-    pygame.draw.circle(screen, (255, 255, 255), (400, 500), 10)
-    # Your game logic and drawing code would go here
+    #m_1 string
+    pygame.draw.line(screen, (207, 184, 124), (origin[0], origin[1]), (r_1[0]+origin[0], r_1[1] + origin[1]), 5)
+    #m_2 string
+    pygame.draw.line(screen, (207, 184, 124), (r_1[0]+origin[0], r_1[1] + origin[1]),(r_2[0]+origin[0], r_2[1] + origin[1]), 5)
+    #m_1 mass
+    pygame.draw.circle(screen, (255, 255, 255), (r_1[0]+origin[0],r_1[1]+origin[1]), 10)
+    #m_2 mass
+    pygame.draw.circle(screen, (255, 255, 255), (r_2[0]+origin[0],r_2[1]+origin[1]), 10)
 
     # Update the display
     pygame.display.flip()
